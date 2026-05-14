714647a21e3cc0fc805a868c82f8e86bc0bb50d4
YASHAVkumar <yashavkumar91@gmail.com>
2026-05-15 02:09:53 +0530

    fix .csproj

---
+++ TestingConsole/Bussiness/AyncProgramming/ApiController.cs
@@ -0,0 +1,19 @@
+// Asynchronous Programming in C#
+
+// Asynchronous programming allows code to run without blocking the main thread.
+
+// Main keywords:
+
+// async
+// await
+
+// Used for:
+
+// API calls
+// database operations
+// file handling
+// network operations
+//Thread.Sleep(5000); Main thread block for 5 seconds 
+//await Task.Delay(5000);
+// Thread is freed while waiting.
+// Application remains responsive.
